abea5f106ff555ba88dc21c600d3a79ba560c473
EddieSource <67773693+EddieSource@users.noreply.github.com>
2020-11-15 18:11:52 -0500

    Add files via upload

---
+++ FindFirstandLastPositionofElementinSortedArray_34.java
@@ -0,0 +1,54 @@
+
+public class FindFirstandLastPositionofElementinSortedArray_34 {
+	
+	public int[] searchRange(int[] nums, int target) {
+		/*
+		 * Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+Follow up: Could you write an algorithm with O(log n) runtime complexity?
+		 */
+        int[] res = {-1,-1}; 
+        if (nums == null || nums.length == 0) {
+            return res; 
+        }
+        
+        
+        int left = 0; 
+        int right = nums.length - 1; 
+        int mid; 
+        while(left < right - 1) {
+            mid = left + (right - left) / 2; 
+            if(target <= nums[mid]){
+                right = mid; 
+            }
+            else {
+                left = mid; 
+            }
+        }
+        
+        if(nums[left] == target){
+            res[0] = left; 
+        }
+        else if(nums[right] == target) {
+            res[0] = right; 
+        }
+        
+        if(res[0] != -1) {
+            int pos = res[0]; 
+            while(pos < nums.length){
+                if(nums[pos] != target) break; 
+                res[1] = pos; 
+                pos++; 
+            }
+            return res; 
+        }
+        return res;  
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
