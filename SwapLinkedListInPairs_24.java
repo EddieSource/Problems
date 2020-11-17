fabc567be5f625800fc143bdcd923c62a010dae6
EddieSource <67773693+EddieSource@users.noreply.github.com>
2020-11-17 17:20:26 -0500

    Update SwapLinkedListInPairs_24.java

--- SwapLinkedListInPairs_24.java
+++ SwapLinkedListInPairs_24.java
@@ -1,4 +1,10 @@
+/*
+ * Given a linked list, swap every two adjacent nodes and return its head.
 
+You may not modify the values in the list's nodes. Only nodes itself may be changed.
+
+ 
+ */
 public class SwapLinkedListInPairs_24 {
 	 public class ListNode {
 		 int val;
