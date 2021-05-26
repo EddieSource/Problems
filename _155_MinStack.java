36578cdda952ede720621090e7a762817ac05202
Xianhui Zhu <xz2715@nyu.edu>
2021-05-26 13:28:23 -0400

    solve problem 155 with optimized space complexity

---
+++ _155_MinStack.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class _155_MinStack {
+    /** initialize your data structure here. */
+	private Deque<Integer> dataStack; 
+	private Deque<Integer> minStack; 
+	private Deque<Integer> sizeStack; 
+	
+    public _155_MinStack() {
+        dataStack = new ArrayDeque<>(); 
+        minStack = new ArrayDeque<>(); 
+        sizeStack = new ArrayDeque<>(); 
+        
+    }
+    
+    public void push(int val) {
+        dataStack.offerFirst(val); 
+        if(minStack.isEmpty() || val < minStack.peekFirst()) {
+        	minStack.offerFirst(val);
+        	sizeStack.offerFirst(dataStack.size());
+        }
+        
+    }
+    
+    public void pop() {
+        int val = dataStack.pollFirst(); 
+        if(sizeStack.peekFirst() > dataStack.size()) {
+        	minStack.pollFirst(); 
+        	sizeStack.pollFirst(); 
+        }
+    }
+    
+    public int top() {
+        return dataStack.peekFirst(); 
+    }
+    
+    public int getMin() {
+    	return minStack.peekFirst(); 
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
