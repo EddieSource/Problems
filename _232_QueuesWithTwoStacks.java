1275ca3595a5b7c4d22e199697bffd05c3bbfcfa
Xianhui Zhu <xz2715@nyu.edu>
2021-05-26 12:33:51 -0400

    solve problem 232

---
+++ _232_QueuesWithTwoStacks.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class _232_QueuesWithTwoStacks {
+    /** Initialize your data structure here. */
+	private Deque<Integer> in; 
+	private Deque<Integer> out; 
+    
+	public _232_QueuesWithTwoStacks() {
+    	in = new ArrayDeque<Integer>(); 
+    	out = new ArrayDeque<Integer>(); 
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        in.offerFirst(x); 
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(out.isEmpty()) {
+        	while(!in.isEmpty()) {
+        		out.offerFirst(in.pollFirst()); 
+        	}
+        }
+        return out.pollFirst(); 
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if(out.isEmpty()) {
+        	while(!in.isEmpty()) {
+        		out.offerFirst(in.pollFirst()); 
+        	}
+        }
+        return out.peekFirst(); 
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return in.size() + out.size() == 0; 
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
