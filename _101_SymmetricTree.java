d5600d7cf1a3b58a7da2716710ef5bdf87e8029e
Xianhui Zhu <xz2715@nyu.edu>
2021-05-27 11:54:45 -0400

    solve problem 101

---
+++ _101_SymmetricTree.java
@@ -0,0 +1,31 @@
+
+
+public class _101_SymmetricTree {
+	public class TreeNode {
+		int val;
+	    TreeNode left;     
+	    TreeNode right;
+	    TreeNode() {}
+	    TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        }
+	}
+	public boolean isSymmetric_helper(TreeNode left, TreeNode right) { 
+		if(left == null && right == null) return true; 
+		else if(left == null || right == null) return false; 
+		else if(left.val != right.val) return false; 
+		else return isSymmetric_helper(left.left, right.right) && isSymmetric_helper(left.right, right.left);
+	}
+    public boolean isSymmetric(TreeNode root) {
+    	if(root == null) return true; 
+        return isSymmetric_helper(root.left, root.right);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
