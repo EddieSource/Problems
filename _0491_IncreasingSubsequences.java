5e66d4650ae8ae78b9ac86e76c540684809d9493
Xianhui Zhu <xz2715@nyu.edu>
2021-06-28 16:12:56 -0400

    correct solution similar to all permutations 2

--- _0491_IncreasingSubsequences.java
+++ _0491_IncreasingSubsequences.java
@@ -5,37 +5,30 @@ public class _0491_IncreasingSubsequences {
         if(nums.length == 0) return res; 
         
         List<Integer> cur = new ArrayList<>(); 
+        
+ 
         DFS(nums, 0, cur, res); 
         return res; 
     }
     
     public void DFS(int[] nums, int index, List<Integer> cur, List<List<Integer>>res) {
-    	if(index == nums.length) {
-    		return; 
+    	if(cur.size() >= 2) {
+    		res.add(new ArrayList<>(cur)); 
     	}
-    	if(cur.isEmpty() || nums[index] >= cur.get(cur.size() - 1)) {
-    		
-    		
-    		cur.add(nums[index]); 
-    		// add since new value has been added
-    		if(cur.size() >= 2) res.add(new ArrayList<>(cur)); 
-    		DFS(nums, index + 1, cur, res); 
-    		cur.remove(cur.size() - 1); 
-    		
-    		// deduplicate
-//    		while(index < nums.length && nums[index] == nums[index]) {
-//    			index++; 
-//    		}
-    		
-    		
-    		while(index < nums.length - 1 && !cur.isEmpty() && cur.get(cur.size() - 1) == nums[index]) {
-    			index++; 
-    		}
-    		
-    		
-    		// no need to add since no new value has been added
-    		DFS(nums, index + 1, cur, res); 
+    	
+    	Set<Integer> s = new HashSet<>(); 
+
+    	for(int i = index; i < nums.length; i++) {
+            // checking if our current picked element is greater than last pushed element in our sub result & our Hashset has no occurence of that element 
+            if((cur.isEmpty() || nums[i] >= cur.get(cur.size() - 1)) && !s.contains(nums[i])){                                                       
+                s.add(nums[i]); 
+                cur.add(nums[i]); 
+                DFS(nums, i + 1, cur, res);      // recurr for next elements and add it our sub_res, if it's satisfying the condition
+                
+                cur.remove(cur.size() - 1);  // BACKTRACKING STEP
+            }
     	}
+    	
     }
     
 	public static void main(String[] args) {
