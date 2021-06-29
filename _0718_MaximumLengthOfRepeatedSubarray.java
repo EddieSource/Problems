746325722118f33a46e003799ef4d033a3d5a03b
Xianhui Zhu <xz2715@nyu.edu>
2021-06-29 12:40:48 -0400

    solve longest common substring by a 2d dp array

---
+++ _0718_MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class _0718_MaximumLengthOfRepeatedSubarray {
+    public int findLength(int[] nums1, int[] nums2) {
+        // assumption nums1.length > 0, nums2.length > 0
+    	// assumption
+        // assumption nums1.length > 0, nums2.length > 0
+    	
+    	int[][] m = new int[nums1.length + 1][nums2.length + 1]; 
+    	//m[i, j] means the length of common subarray from [0, i) of nums1, and [0, j) from nums2
+    	int solution = 0; 
+        
+    	for (int i = 0; i <= nums1.length; i++) {
+    		for (int j = 0; j <= nums2.length; j++) {
+    			// initial status
+    			if(i == 0) m[i][j] = 0; 
+    			else if (j == 0) m[i][j] = 0; 
+    			
+    			// induction rule
+    			else {
+    				if(nums1[i - 1] == nums2[j - 1]) {
+                        m[i][j] = 1 + m[i - 1][j - 1]; 
+                        solution = Math.max(m[i][j], solution); 
+                    }
+    				else m[i][j] = 0; 
+    			}
+    		}
+    	}
+    	
+    	
+    	return solution;
+         
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
