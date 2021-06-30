682aed75d899bac27b39ed4cce7c32d2bb58eaba
Xianhui Zhu <xz2715@nyu.edu>
2021-06-30 13:29:09 -0400

    solve problem 1547 with 2d dp array

---
+++ _1547_MinimumCostToCutAStick.java
@@ -0,0 +1,36 @@
+import java.util.*; 
+public class _1547_MinimumCostToCutAStick {
+    public int minCost(int n, int[] cuts) {
+    Arrays.sort(cuts); 
+    int[] helper = new int[cuts.length + 2]; 
+    helper[0] = 0; 
+    for (int i = 0; i < cuts.length; i++) {
+      helper[i + 1] = cuts[i]; 
+    }
+    helper[helper.length - 1] = n; 
+
+    int[][] minCost = new int[helper.length][helper.length]; 
+    // minCost represent the minimum cost of cutting the wood between i and j
+    for (int i = 1; i < helper.length; i++) {
+      for (int j = i - 1; j >= 0; j--) {
+        if (j + 1 == i) {
+          minCost[j][i] = 0; 
+        }
+        else {
+          minCost[j][i] = Integer.MAX_VALUE; 
+          for (int k = j + 1; k <= i - 1; k++) {
+            minCost[j][i] = Math.min(minCost[j][i], minCost[j][k] + minCost[k][i]); 
+          }
+          minCost[j][i] += helper[i] - helper[j]; 
+        }
+      }
+    }
+    return minCost[0][helper.length - 1]; 
+
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- testing.java
+++ testing.java
@@ -1,9 +1,36 @@
 
 public class testing {
+	public static int numIncreasingSubsequences(int[] a) {
+	    // Write your solution here
+	    if (a.length == 0) return 0; 
+	    int[] m = new int[a.length]; 
+	    // m[i] represents the count of ascending subsequence ending at i
+	    
+	    m[0] = 1; 
+	    
+	    // initial status
+	    
+	    int count = 1; 
 
+	    for (int i = 1; i < a.length; i++) {
+	      for (int j = 0; j < i; j++) {
+	    	// letter itself is an ascending array
+	    	m[i] = 1; 
+	        if (a[i] > a[j]) {// ascending
+	          m[i] = m[i] + m[j]; 
+	        } 
+	        
+	        // if not we don't do anything just reset max SubLength
+	        count += m[i]; 
+	      }
+	    }
+
+	    return count; 
+	  
+	  }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(numIncreasingSubsequences(new int[] {1,2,3})); 
 	}
 
 }
