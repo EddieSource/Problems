b379861e74997b10ded98812cb1a1aa9a72f93fb
Xianhui Zhu <xz2715@nyu.edu>
2021-07-20 00:38:46 -0400

    array hopper 4

--- testing.java
+++ testing.java
@@ -1,61 +1,67 @@
 import java.util.*; 
 public class testing {
-	public int kthSum(int[] A, int[] B, int k) {
+	public int minJump(int[] array, int index) {
 	    // Write your solution here
-
-	    // assumption K > 0 && K <= m * n
-	    if(A == null || B == null) return -1; 
-
-	    int a = 0, b = 0;  
-	    while(a < A.length && b < B.length){
-	      // a and b must be in the 
-	      int sum = A[a] + B[b]; 
-	      k--; 
-	      if(k == 0) return sum; 
-
-	      if(A[a] < B[b]) a++; 
-	      else b++; 
-	    }
-
-	    while(a < A.length){
-	      // A left
-	      int sum = A[a] + B[B.length - 1]; 
-	      k--; 
-	      if(k == 0) return sum; 
-	      a++; 
-	    }
-	    
-	    while(b < B.length){
-	      // B left
-	      int sum = A[A.length - 1] + B[b]; 
-	      k--; 
-	      if(k == 0) return sum; 
-	      b++; 
-	    }
-
-	    return -1; 
-	    
+	      boolean[] done = new boolean[array.length]; 
+	    	Arrays.fill(done, false); // all the indices are not start processing
+	    	int[] minStep = new int[array.length]; 
+	    	Arrays.fill(minStep, -1); 
+	    	return minJumpHelper(array, index, done, minStep); 
 	  }
+	    public int minJumpHelper(int[] arr, int start, boolean[] done, int[] minStep) {
+	        // from start reach to any index with value zero
+	    	
+	    	// base case
+	    	if(start < 0 || start >= arr.length) return -1; 
+	      if(start == arr.length - 1) {
+	        minStep[start] = 0; 
+	        done[start] = true; 
+	        return minStep[start]; 
+	      }
+	    	
+	    	// deduplicate: if we are processing/processed before we return immediately
+	    	if(done[start]) return minStep[start]; 
+	    	
+	      // recursion rule
+	    	minStep[start] = -1; 
+	    	done[start] = true; // prevent the cycle, start state of the current node
+
+
+	    	// arr[start] must not be 0 now so we start from 1
+	      for(int step = 0; step <= arr[start]; step++){
+	        int left = minJumpHelper(arr, start + step, done, minStep); 
+	        // left == -1 or >= 0
+	        int right = minJumpHelper(arr, start - step, done, minStep); 
+	        
+	        int cur; 
+	        if(left == -1 && right == -1) cur = -1; 
+	        else if(left == -1) cur = right + 1; 
+	        else if(right == -1) cur = left + 1; 
+	        else cur = Math.min(left, right) + 1; 
+	        
+	        if( cur != -1) {
+		        if(minStep[start] == -1) {
+			          // if can not reach we update the minStep[start]
+			          minStep[start] = cur; 
+			        }
+
+			    else {
+			       // if minStep can reach we find the min
+			       minStep[start] = Math.min(cur, minStep[start]); 
+			    }
+	        }
+	        
+
+	      }
+
+	    	done[start] = true; // end state of the current node
+
+	    	return minStep[start]; 
+	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Map<List<Integer>, Boolean> a = new HashMap<>(); 
-		
-		List<Integer> b = new ArrayList<>(); 
-		b.add(1); 
-		b.add(2); 
-		
-		List<Integer> c = new ArrayList<>(); 
-		c.add(1); 
-		c.add(2); 
-		a.put(b, true); 
-		a.put(c, false); 
-		
-		List<Integer> d = new ArrayList<>(); 
-		d.add(1); 
-		d.add(2);
-		
-		System.out.println(a.get(b)); 
-		System.out.println(a.containsKey(d)); 
+		testing a = new testing(); 
+		a.minJump(new int[] {1, 2, 0}, 0); 
 		
 	}
 
